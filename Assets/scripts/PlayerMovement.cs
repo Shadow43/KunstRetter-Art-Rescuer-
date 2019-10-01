35c9d2d7e09357e07f218e59f8de8a0eddf32c63
ShadowFox43 <shadowfox_43@hotmail.com>
2019-10-01 13:25:21 +0800

    Player movement commit.

---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D PlayerRigidBody;
+    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void fixedUpdate()
+    {
+        if (PlayerRigidBody != null)
+        {
+            ApplyInput();
+        }
+
+        else
+        {
+            Debug.LogWarning("Rigid body not attached to player" + gameObject.name);
+        }
+    }
+
+    //Check to see if arrow keys are pressed and move character
+
+    public void ApplyInput()
+    {
+        float xInput = Input.GetAxis("x");
+        float yInput = Input.GetAxis("y");
+
+        float xForce = xInput * moveSpeed * Time.deltaTime;
+        Vector2 force = new Vector2(xForce, 0);
+        PlayerRigidBody.AddForce(force);
+
+        Debug.Log("xForce: " + xForce);
+        Debug.Log("VelocityplayerRigidbody " + PlayerRigidBody.velocity);
+    }
+}
